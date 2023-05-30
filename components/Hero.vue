039d0fe38a5db35ca48786c0053bfe44533a65de
Vantol Bennett <vvantol2000@icloud.com>
2023-05-30 14:30:53 -0400

    feat: added Hero.vue and PostCard.vue

---
+++ components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div class="py-12 bg-gray-100 md:py-24">
+		  <div
+			class="grid max-w-screen-xl px-6 mx-auto lg:px-8 xl:px-4 md:grid-cols-4 xl:grid-cols-5 gap-x-12 lg:gap-x-20"
+		  >
+			<div class="self-center order-2 col-span-2 mt-12 md:order-1 md:mt-0">
+			  <h1
+				class="mb-2 text-3xl font-bold text-gray-800 md:text-4xl lg:text-5xl md:mb-4 lg:mb-8"
+			  >
+				Vantol R. Bennett
+			  </h1>
+			  <p class="mb-6 text-sm text-gray-400 xl:text-xl lg:mb-8 xl:mb-10">
+				Jamaican Educator & Developer at (MPS), Founder of Zhyjenae
+			  </p>
+			  <h1
+				class="mb-2 text-3xl font-bold text-gray-800 md:text-4xl lg:text-5xl md:mb-4 lg:mb-8"
+			  >
+				The best way to write code all day...
+			  </h1>
+			  <p class="mb-6 text-lg text-gray-600 xl:text-xl lg:mb-8 xl:mb-10">
+				is by doing it! Evolve with innovation
+			  </p>
+			</div>
+			<div class="order-1 col-span-2 md:order-2 xl:col-span-3">
+			  <img
+				src="https://res.cloudinary.com/ddszyeplg/image/upload/v1656478354/IMG_1412_orxemy.jpg"
+				class="rounded-lg shadow-2xl"
+				alt="Vantol Bennett"
+			  />
+			</div>
+		  </div>
+		</div>
+	</div>
+</template>
